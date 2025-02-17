28f6944560d4aca2f6dbc2be7ecda4e925f69f00
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-02-17 21:13:26 +0530

    Intialize backend with spring boot

---
+++ server/src/main/java/com/rishav/Eclipser/EclipserApplication.java
@@ -0,0 +1,13 @@
+package com.rishav.Eclipser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclipserApplication.class, args);
+	}
+
+}
