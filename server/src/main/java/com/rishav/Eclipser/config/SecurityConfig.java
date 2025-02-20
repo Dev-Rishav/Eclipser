d17f4aa96f3b533a26754b7cac794ed08930262d
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-02-20 15:52:05 +0530

    create and the register user POST method

--- server/src/main/java/com/rishav/Eclipser/config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.rishav.Eclipser.config;
-
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.server.authentication.WebFilterChainServerAuthenticationSuccessHandler;
-
-@Configuration
-public class SecurityConfig extends WebFilterChainServerAuthenticationSuccessHandler {
-}
