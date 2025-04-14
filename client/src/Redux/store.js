a17278c4bcce97118d78210b30982932f612c42a
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2025-04-15 00:39:08 +0530

    Drop state mangament for socket instances

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,12 +1,10 @@
 import authReducer from './reducers/authReducer';
 import {configureStore} from "@reduxjs/toolkit"
-import socketReducer from './reducers/socketReducer';
 
 
 const store=configureStore({
     reducer:{
         auth:authReducer,
-        socket:socketReducer,
     },
 });
 
